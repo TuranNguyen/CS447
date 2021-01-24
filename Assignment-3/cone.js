d047816ec26150fb2d559626b5158534474aa03e
AllUser42 <alluser@protonmail.ch>
2021-01-23 16:17:51 -0800

    added in conejs files for asssignment 3

---
+++ Assignment-3/cone.js
@@ -0,0 +1,27 @@
+var gl = null;
+var cone = null;
+
+function init() {
+    var canvas = document.getElementById("webgl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+
+    if (!gl) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    //gl.clearColor(1.0, 0.0, 1.0, 1.0);
+    //gl.enable(gl.DEPTH_TEST)
+    cone = new Cone(gl, 8)
+
+    render();
+}
+
+function render() {
+    //gl.clear(gl.COLOR_BUFFER_BIT);
+    cone.render()
+
+}
+
+window.onload = init;
